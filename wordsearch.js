1d421dcbc6767e127a385bab91dd8109d4cc295d
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-27 21:50:38 -0800

    code added

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const { transpose } = require("../../transpose");
 
-module.exports = wordSearch
+const wordFinder = (arr, word) => {
+  for (let i of arr) {
+    let concatWord = "";
+    for (let j of i) {
+      concatWord = concatWord + j;
+      if (concatWord === word) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+const wordSearch = (letters, word) => {
+  if (wordFinder(letters, word) || wordFinder(transpose(letters), word)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = wordSearch;
